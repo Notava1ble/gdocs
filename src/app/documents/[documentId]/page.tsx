29d8260ff8da3555047c200db481527d8378f8a2
Visar <visarmullaj07@gmail.com>
2025-04-20 10:17:33 +0200

    Add Zustand for state management and implement Editor store and Toolbar components

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,13 +1,16 @@
 import Editor from "./editor";
+import Toolbar from "./toolbar";
 
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
 }
 
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
-  const documentId = (await params).documentId;
+  console.log(params);
+
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
+      <Toolbar />
       <Editor />
     </div>
   );
